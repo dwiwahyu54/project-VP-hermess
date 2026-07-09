d32f828a261672e49b6335c8c2c6224400d613ed
dwiwahyu54 <nadhifautama54@gmail.com>
2026-07-09 14:35:34 +0700

    Dashboard: samakan header tabel Vessel Activity 100% dengan Excel

--- src/App.jsx
+++ src/App.jsx
@@ -2066,6 +2066,7 @@ function Dashboard({ reports, onNew, user }) {
               <th style={ss.th} colSpan={2}>ME (MAIN ENGINE) - AE AT SEA</th>
               <th style={ss.th} colSpan={2}>ME - AE AT PORT</th>
               <th style={ss.th} colSpan={3}>KONSUMSI BBM (LITRE)</th>
+              <th style={ss.th} colSpan={3}>KONSUMSI BBM (LITRE)</th>
               <th style={ss.th} rowSpan={2}>AVG/F (JUNE)</th>
             </tr>
             <tr>
@@ -2080,6 +2081,9 @@ function Dashboard({ reports, onNew, user }) {
               <th style={ss.th}>ME</th>
               <th style={ss.th}>AE AT SEA</th>
               <th style={ss.th}>AE AT PORT</th>
+              <th style={ss.th}>ME</th>
+              <th style={ss.th}>AE AT SEA</th>
+              <th style={ss.th}>AE AT PORT</th>
             </tr>
           </thead>
           <tbody>
@@ -2087,6 +2091,11 @@ function Dashboard({ reports, onNew, user }) {
               <tr key={ship}>
                 <td style={ss.td(idx%2)}>{idx+1}</td>
                 <td style={{ ...ss.td(idx%2), fontWeight:600, whiteSpace:"nowrap", minWidth:120 }}>{ship}</td>
+                <td style={ss.td(idx%2)}></td>
+                <td style={ss.td(idx%2)}></td>
+                <td style={ss.td(idx%2)}></td>
+                <td style={ss.td(idx%2)}></td>
+                <td style={ss.td(idx%2)}></td>
                 <td style={ss.td(idx%2)}></td>
                 <td style={ss.td(idx%2)}></td>
                 <td style={ss.td(idx%2)}></td>

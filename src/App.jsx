2711c91269fd1fc730587747931dc83636556fe2
dwiwahyu54 <nadhifautama54@gmail.com>
2026-07-13 03:28:26 +0000

    fix: remove TOTAL row from ReportLog (Semua Laporan)

--- src/App.jsx
+++ src/App.jsx
@@ -2849,17 +2849,7 @@ function ReportLog({ reports, onView, user }) {
               );
             })}
           </tbody>
-          <tfoot>
-            <tr style={{ background:`${C.muted}18`, fontWeight:800, letterSpacing:"0.06em", fontSize:10, color:C.muted }}>
-              <td style={{ textAlign:"center", border:`1px solid ${C.border}`, padding:"7px 9px" }}>TOTAL</td>
-              <td style={{ border:`1px solid ${C.border}`, padding:"7px 9px" }}></td>
-              <td style={ss.td(1)}></td>
-              <td style={ss.td(1)}></td>
-              <td style={ss.td(1)}></td>
-              <td style={ss.td(1)}></td>
-              <td style={ss.td(1)}></td>
-            </tr>
-          </tfoot>
+
         </table>
       </div>
     </div>
